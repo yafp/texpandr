1b64c169bbc350eec19d4c00ba380a2e1d4c1506
fidel <fidel@yafp.de>
2016-11-18 23:01:07 +0100

    20161118 - Adding a first version of the script. Changes from texpander: hides header

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # texpandr
 A simple bash based text-expander for Linux
+
+
+## Getting started
+- Download the latest version from https://github.com/yafp/texpandr/archive/master.zip
+- Extract the archive
+- Copy texpandr.sh to your desired location
+- Define a shortcut to trigger/execute texpandr.sh
+- Create a note directory .texpandr in your HOME directory
+- Add an example note in .texpandr with the name DEMO.TXT and add the text it should expand to the file
+- Execute .texpandr via your defined shortcut
+
+
+## Credit
+Texpandr is inspired by Texpander (https://github.com/leehblue/texpander)
---
+++ texpandr.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# NAME:				Texpandr
+# FUNCTION:			A simple bash based text-expander for Linux
+# URL:				https://github.com/yafp/texpandr/
+# Inspired by: 		https://github.com/leehblue/texpander
+
+APPNAME="Texpandr"
+APPVERSION="20161118.01"
+
+base_dir="${HOME}/.texpandr/"
+abbrvs=$(ls $base_dir)
+name=$(zenity --list --title="$APPNAME" --hide-header --column=Abbreviations $abbrvs)
+
+path=$base_dir$name
+
+if [[ $name ]]
+then
+  pid=$(xdotool getwindowfocus getwindowpid)
+  proc_name=$(cat /proc/$pid/comm)
+
+  if [ -e "$path" ]
+  then
+    clipboard=$(xclip -selection clipboard -o)
+    xclip -selection c -i "$path"
+
+    if [[ $proc_name =~ ^(terminal|terminator) ]]
+    then
+      xdotool key ctrl+shift+v
+    else
+      xdotool key ctrl+v
+    fi
+
+    echo $clipboard | xclip -selection c
+  else
+    zenity --error --text="Abbreviation not found:\n$name"
+  fi
+fi
